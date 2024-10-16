8a8131729c253c800bb0f1326d1403291371b6e0
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2024-10-16 17:10:59 +0200

    calibrate 360

--- charuco_calibration.py
+++ charuco_calibration.py
@@ -15,7 +15,10 @@ SQUARES_HORIZONTALLY = 7
 SQUARE_LENGTH = 0.10 # in meters
 MARKER_LENGTH = 0.08  # in meters
 # ...
-PATH_TO_YOUR_IMAGES = 'testdata/charuco_calib_set_1/'
+#PATH_TO_YOUR_IMAGES = 'testdata/charuco_calib_set_1/'
+PATH_TO_YOUR_IMAGES = 'testdata/charuco_calib_gopro_max_100/'
+#ecl_list = ["GOPR0026.JPG"]
+ecl_list  = ["GS__0010_90.JPG", "GS__0014_90.JPG"]
 print(f"{ARUCO_DICT=}")
 # ------------------------------
 def get_calibration_parameters(img_dir, exclude_list=["GOPR0026.JPG"]):
@@ -54,7 +57,9 @@ def get_calibration_parameters(img_dir, exclude_list=["GOPR0026.JPG"]):
                 print("ids not determined")
         else:
             print("no markers found")
-    
+    if len(all_charuco_corners) == 0:
+        print("no corners detected at all")
+        return None
     # Calibrate camera with extracted information
     result, mtx, dist, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(all_charuco_corners, all_charuco_ids, board, imgSize, None, None)
     return mtx, dist
@@ -63,7 +68,11 @@ SENSOR = 'gopro_2'
 LENS = 'gopro_2'
 OUTPUT_JSON = 'calibration.json'
 
-mtx, dist = get_calibration_parameters(img_dir=PATH_TO_YOUR_IMAGES)
+ret = get_calibration_parameters(img_dir=PATH_TO_YOUR_IMAGES, exclude_list=ecl_list)
+if ret is None:
+    print("no calibration could be estimated")
+    exit(1)
+mtx, dist = ret
 data = {"sensor": SENSOR, "lens": LENS, "mtx": mtx.tolist(), "dist": dist.tolist()}
 
 with open(OUTPUT_JSON, 'w') as json_file:
@@ -73,49 +82,50 @@ print(f'Data has been saved to {OUTPUT_JSON}')
 
 json_file_path = './calibration.json'
 
-with open(json_file_path, 'r') as file: # Read the JSON file
-    json_data = json.load(file)
+# with open(json_file_path, 'r') as file: # Read the JSON file
+#     json_data = json.load(file)
 
-mtx = np.array(json_data['mtx'])
-dst = np.array(json_data['dist'])
+# mtx = np.array(json_data['mtx'])
+# dst = np.array(json_data['dist'])
 
-image_path = [os.path.join(PATH_TO_YOUR_IMAGES, f) for f in os.listdir(PATH_TO_YOUR_IMAGES) if f.endswith(".JPG")][4] # NOTE select image to calculate size
-image = cv2.imread(image_path)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# image_path = [os.path.join(PATH_TO_YOUR_IMAGES, f) for f in os.listdir(PATH_TO_YOUR_IMAGES) if f.endswith(".JPG")][0] # NOTE select image to calculate size
+# image = cv2.imread(image_path)
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-h,  w = image.shape[:2]
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dst, (w,h), 1, (w,h))
-image = cv2.undistort(image, mtx, dst, None, newcameramtx)
+# h,  w = image.shape[:2]
+# newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dst, (w,h), 1, (w,h))
+# image = cv2.undistort(image, mtx, dst, None, newcameramtx)
 
-all_charuco_ids = []
-all_charuco_corners = []
+# all_charuco_ids = []
+# all_charuco_corners = []
 
-dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT)
-board = cv2.aruco.CharucoBoard((SQUARES_VERTICALLY, SQUARES_HORIZONTALLY), SQUARE_LENGTH, MARKER_LENGTH, dictionary)
-params = cv2.aruco.DetectorParameters()
-detector = cv2.aruco.ArucoDetector(dictionary, params)
+# dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT)
+# board = cv2.aruco.CharucoBoard((SQUARES_VERTICALLY, SQUARES_HORIZONTALLY), SQUARE_LENGTH, MARKER_LENGTH, dictionary)
+# params = cv2.aruco.DetectorParameters()
+# detector = cv2.aruco.ArucoDetector(dictionary, params)
 
-marker_corners, marker_ids, rejectedCandidates = detector.detectMarkers(image)
-if marker_ids is not None and len(marker_ids) > 0: # If at least one marker is detected
-    # cv2.aruco.drawDetectedMarkers(image_copy, marker_corners, marker_ids)
-    ret, charucoCorners, charucoIds = cv2.aruco.interpolateCornersCharuco(marker_corners, marker_ids, image, board)
-    if charucoCorners is not None and charucoIds is not None and len(charucoCorners) > 3:
-        all_charuco_corners.append(charucoCorners)
-        all_charuco_ids.append(charucoIds)
+# marker_corners, marker_ids, rejectedCandidates = detector.detectMarkers(image)
+# if marker_ids is not None and len(marker_ids) > 0: # If at least one marker is detected
+#     # cv2.aruco.drawDetectedMarkers(image_copy, marker_corners, marker_ids)
+#     ret, charucoCorners, charucoIds = cv2.aruco.interpolateCornersCharuco(marker_corners, marker_ids, image, board)
+#     if charucoCorners is not None and charucoIds is not None and len(charucoCorners) > 3:
+#         all_charuco_corners.append(charucoCorners)
+#         all_charuco_ids.append(charucoIds)
 
-    retval, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(np.array(all_charuco_corners)[0], np.array(all_charuco_ids)[0], board, np.array(mtx), np.array(dst), np.empty(1), np.empty(1))
+#     retval, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(np.array(all_charuco_corners)[0], np.array(all_charuco_ids)[0], board, np.array(mtx), np.array(dst), np.empty(1), np.empty(1))
 
-    Zx, Zy, Zz = tvec[0][0], tvec[1][0], tvec[2][0]
-    fx, fy = mtx[0][0], mtx[1][1]
+#     Zx, Zy, Zz = tvec[0][0], tvec[1][0], tvec[2][0]
+#     fx, fy = mtx[0][0], mtx[1][1]
 
-    print(f'Zz = {Zz}\nfx = {fx}')
-else:
-    print("no marker for pose estimation detected")
+#     print(f'Zz = {Zz}\nfx = {fx}')
+# else:
+#     print("no marker for pose estimation detected")
+#     exit(1)
 
 
-# size in meters
-def perspective_function(x, Z, f): 
-    return x*Z/f
+# # size in meters
+# def perspective_function(x, Z, f): 
+#     return x*Z/f
 
-nb_pixels = 192
-print(perspective_function(nb_pixels, Zz, fx))
+# nb_pixels = 192
+# print(perspective_function(nb_pixels, Zz, fx))
---
+++ convert_equirect_image_to_pinhole.py
@@ -0,0 +1,66 @@
+import numpy as np
+from PIL import Image
+from omnicv import fisheyeImgConv
+import math
+
+def convert(inname, outname):
+
+    equiRect = np.array(Image.open(inname))
+
+    outShape = [400, 400]
+    inShape = equiRect.shape[:2]
+    mapper = fisheyeImgConv()
+    FOV = 90
+    Theta = 45 # horizontal shift
+    Phi = 0 # vertical shift in deg
+    Hd = outShape[0]
+    Wd = outShape[1]
+    np.bool = np.bool_
+    np.float = np.float64
+    persp = mapper.eqruirect2persp(equiRect, FOV, Theta, Phi, Hd, Wd)
+
+    def get_block(equiRect, FOV=90, Theta=0, Phi = 0, outShape = [800, 800]):
+        inShape = equiRect.shape[:2]
+        mapper = fisheyeImgConv()
+        #FOV = 90
+        #Theta = 45 # horizontal shift
+        #Phi = 0 # vertical shift in deg
+        Hd = outShape[0]
+        Wd = outShape[1]
+        import numpy as np
+        np.bool = np.bool_
+        np.float = np.float64
+        persp = mapper.eqruirect2persp(equiRect, FOV, Theta, Phi, Hd, Wd)
+        return Image.fromarray(persp)
+
+    img = get_block(equiRect, 90, 0,0)
+    img.save(outname,"PNG")
+    
+import sys
+import os
+    
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script_name.py <indir> <outdir>")
+        sys.exit(1)
+
+    indir = sys.argv[1]
+    outdir = sys.argv[2]
+
+    if not os.path.exists(indir):
+        print("Error: Input directory does not exist")
+        sys.exit(1)
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    for filename in os.listdir(indir):
+        infile = os.path.join(indir, filename)
+        if os.path.isfile(infile):
+            base, extension = os.path.splitext(filename)
+            outname = base + "_90" + extension
+            outpath = os.path.join(outdir, outname)
+            convert(infile, outpath)
+
+if __name__ == "__main__":
+    main()
